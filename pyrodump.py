f0b1bd3b73f5007897c61605f203c831c82c9a72
KIM YU JUN <awakening95@naver.com>
2019-01-23 02:07:05 +0900

    Modify pyrodump.py

--- pyrodump.py
+++ pyrodump.py
@@ -3,6 +3,7 @@ import time
 import curses
 import analysis
 import pcapy
+import sys
 from threading import Thread, Lock
 from _datetime import datetime
 
@@ -26,7 +27,7 @@ def hopping_channel():
         elif num == 13:
             num = 0
         lock.release()
-        time.sleep(0.5)
+        time.sleep(0.3)
 
 
 def get_ap_list(pkt):
@@ -80,48 +81,58 @@ def get_ap_list(pkt):
                         break
 
 
-start_time = datetime.now()
-elapsed_time = 0
+def main(interface):
+    start_time = datetime.now()
 
-t = Thread(target=hopping_channel)
-t.start()
+    t = Thread(target=hopping_channel)
+    t.start()
 
-scr = curses.initscr()
+    scr = curses.initscr()
 
-while True:
-    scr.resize(200, 120)
+    while True:
+        scr.resize(200, 120)
 
-    pcap = pcapy.open_live("mon0", 1000, 0, 0)  # get packet capture descriptor
-    pkt = pcap.next()[1]
+        pcap = pcapy.open_live(interface, 1024, 0, 0)  # get packet capture descriptor
+        pkt = pcap.next()[1]
 
-    get_ap_list(pkt)
+        get_ap_list(pkt)
 
-    now_time = datetime.now()
-    elapsed_time = int((now_time - start_time).total_seconds())
+        now_time = datetime.now()
+        elapsed_time = int((now_time - start_time).total_seconds())
 
-    if elapsed_time < 60:
-        scr.addstr(0, 0, "CH  %s ][ Elapsed: %s s ][ %s-%s-%s %s:%s" % (
-            hopping_ch[num], elapsed_time, now_time.year, now_time.month, now_time.day, now_time.hour, now_time.minute))
+        if elapsed_time < 60:
+            scr.addstr(0, 0, "CH  %s ][ Elapsed: %s s ][ %s-%s-%s %s:%s" % (
+                hopping_ch[num], elapsed_time, now_time.year, now_time.month, now_time.day, now_time.hour,
+                now_time.minute))
 
-    elif 60 <= elapsed_time < 3600:
-        scr.addstr(0, 0, "CH  %s ][ Elapsed: %s mins ][ %s-%s-%s %s:%s" % (
-            hopping_ch[num], int(elapsed_time / 60), now_time.year, now_time.month, now_time.day, now_time.hour, now_time.minute))
+        elif 60 <= elapsed_time < 3600:
+            scr.addstr(0, 0, "CH  %s ][ Elapsed: %s mins ][ %s-%s-%s %s:%s" % (
+                hopping_ch[num], int(elapsed_time / 60), now_time.year, now_time.month, now_time.day, now_time.hour,
+                now_time.minute))
 
-    elif elapsed_time >= 3600:
-        scr.addstr(0, 0, "CH  %s ][ Elapsed: %s hours ][ %s-%s-%s %s:%s" % (
-            hopping_ch[num], int(elapsed_time / 3600), now_time.year, now_time.month, now_time.day, now_time.hour, now_time.minute))
+        elif elapsed_time >= 3600:
+            scr.addstr(0, 0, "CH  %s ][ Elapsed: %s hours ][ %s-%s-%s %s:%s" % (
+                hopping_ch[num], int(elapsed_time / 3600), now_time.year, now_time.month, now_time.day, now_time.hour,
+                now_time.minute))
 
-    scr.addstr(2, 0, "BSSID              PWR  Beacons    #Data, #/s  CH  MB   ENC  CIPHER AUTH ESSID")
+        scr.addstr(2, 0, "BSSID              PWR  Beacons    #Data, #/s  CH  MB   ENC  CIPHER AUTH ESSID")
 
-    if len(ap_list) != 0:
-        for i in range(len(ap_list)):
-            scr.addstr(4 + i, 0,
-                       "%s" % ap_list[i][0] + "%5s" % ap_list[i][1] + "%9s" % ap_list[i][2] +  # BSSID, PWR, Beacons
-                       "%9s" % ap_list[i][3] + "%5s" % ap_list[i][4] + "%4s  " % ap_list[i][5] +  # #Data, #/s, CH
-                       "%-5s" % ap_list[i][6] + "%-5s" % ap_list[i][7] + "%-7s" % ap_list[i][8] +  # MB, ENC, CIPHER
-                       "%-5s" % ap_list[i][9] + "%s" % ap_list[i][10])  # AUTH, ESSID
+        if len(ap_list) != 0:
+            for i in range(len(ap_list)):
+                scr.addstr(4 + i, 0,
+                           "%s" % ap_list[i][0] + "%5s" % ap_list[i][1] + "%9s" % ap_list[i][2] +  # BSSID, PWR, Beacons
+                           "%9s" % ap_list[i][3] + "%5s" % ap_list[i][4] + "%4s  " % ap_list[i][5] +  # #Data, #/s, CH
+                           "%-5s" % ap_list[i][6] + "%-5s" % ap_list[i][7] + "%-7s" % ap_list[i][8] +  # MB, ENC, CIPHER
+                           "%-5s" % ap_list[i][9] + "%s" % ap_list[i][10])  # AUTH, ESSID
 
-    scr.addstr(5 + len(ap_list), 0, "BSSID              STATION            PWR   Rate    Lost    Frames  Probe")
+        scr.addstr(5 + len(ap_list), 0, "BSSID              STATION            PWR   Rate    Lost    Frames  Probe")
 
-    scr.refresh()
-    scr.erase()
+        scr.refresh()
+        scr.erase()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        main(sys.argv[1])
+    else:
+        print("syntax: python pyrodump.py <interface>")
