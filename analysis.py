b3c0e624f83d39fb533e8d72d1b40bc20671c64d
KIM YU JUN <awakening95@naver.com>
2019-01-23 06:45:34 +0900

    Modify files

--- analysis.py
+++ analysis.py
@@ -2,12 +2,10 @@
 해당 프로그램은 airodump와 유사한 기능을 구현하기 위해  Beacon Frame, Probe Request, 
 Probe Response, Data Frame에 대한 정보를 이용하며 나머지 Frame에 대한 정보는 구하지 않는다.
 """
-channel_dict = {2412: 1, 2417: 2, 2422: 3, 2427: 4, 2432: 5, 2437: 6, 2442: 7, 2447: 8, 2452: 9, 2457: 10, 2462: 11, 2467: 12, 2472: 13, 2484: 14}
 
 
-def get_channel(pkt, present_flag_list, base_length):
-    channel_location = base_length + present_flag_list[0] * 8 + present_flag_list[1] * 1 + present_flag_list[2] * 1
-    channel = channel_dict[int.from_bytes(pkt[channel_location:channel_location+2], byteorder='little')]
+def get_channel(pkt, i, length):
+    channel = pkt[i+1+length]
 
     return channel
 
@@ -20,11 +18,15 @@ def get_pwr(pkt, present_flag_list, base_length):
     return pwr
 
 
-def get_type(type):
+def get_type(pkt, mac_header):
+    type = (pkt[mac_header] & 0b00001100) >> 2
+
     return {0: "Management", 2: "Data"}.get(type, "Other")
 
 
-def get_mgt_sub_type(sub_type):
+def get_mgt_sub_type(pkt, radiotap_length):
+    sub_type = (pkt[radiotap_length] & 0b11110000) >> 4
+
     return {4: "ProbeReq", 5: "ProbeResp", 8: "Beacon"}.get(sub_type, "Other")
 
 
@@ -35,12 +37,6 @@ def get_data_sub_type(pkt, radiotap_length):
             4: "Null", 5: "CF ACK", 6: "CF Poll", 7: "CF ACK + CF Poll",
             8: "QoS data", 9: "QoS data + CF Ack", 10: "QoS data + CF Poll", 11: "QoS data + CF Ack + CF Poll",
             12: "QoS Null", 14: "QoS CF Poll", 15: "QoS CF Ack + CF Pol"}.get(sub_type, "Other")
-
-
-def get_is_qos_data(pkt, radiotap_length):
-    is_qos_data = (pkt[radiotap_length] & 0b10000000) >> 7
-
-    return {0: False, 1:True}.get(is_qos_data, False)
 
 
 def get_to_ds(pkt, radiotap_length):
@@ -173,6 +169,7 @@ def get_id(id):
     return {
         0: "SSID",
         1: "Rates",
+        3: "DSSet",
         48: "RSN",
         50: "ERates",
         221: "Vender"
@@ -201,20 +198,21 @@ def get_auth(akm):
 class Dot11:
     def __init__(self, pkt):
         # It is available variables
+        self.channel = 1
         self.pwr = 0
-        self.ssid = ""
-        self.ch = ""
-        self.max_rate = 0
-        self.mb = ""
         self.addr1 = ""
         self.addr2 = ""
         self.addr3 = ""
         self.addr4 = ""
+        self.max_rate = 0
+        self.qos = ""
         self.preamble = ""
+        self.mb = ""
         self.enc = ""
         self.cipher = ""
         self.auth = ""
         self.is_qos_data = False
+        self.ssid = ""
 
         # radiotap info
         self.radiotap_length = int.from_bytes(pkt[2:4], byteorder='little')
@@ -233,16 +231,14 @@ class Dot11:
 
         self.base_length = 8 + (4 * self.ext_flag_count)  # version, pad, len, present, 확장된 present 각각의 길이를 합한 길이
 
-        # self.channel = get_channel(pkt, self.present_flag_list, self.base_length)
         self.pwr = get_pwr(pkt, self.present_flag_list, self.base_length)
 
         # mac info
         self.mac_header = self.radiotap_length
-
-        self.type = get_type((pkt[self.mac_header] & 0b00001100) >> 2)
+        self.type = get_type(pkt, self.mac_header)
 
         if self.type == "Management":
-            self.sub_type = get_mgt_sub_type((pkt[self.radiotap_length] & 0b11110000) >> 4)
+            self.sub_type = get_mgt_sub_type(pkt, self.radiotap_length)
             self.addr1, self.addr2, self.addr3 = get_mgt_addr(pkt, self.radiotap_length)
 
             self.fixed_mac_body = self.mac_header + 24
@@ -251,10 +247,10 @@ class Dot11:
             self.i = self.variable_mac_body
 
             if self.sub_type == "Beacon" or self.sub_type == "ProbeResp":
-                self.channel = get_channel(pkt, self.present_flag_list, self.base_length)
                 self.preamble = get_preamble(pkt, self.fixed_mac_body)
 
-                while self.i < len(pkt):
+                self.length = 0
+                while self.i + 2 + self.length <= len(pkt):
                     self.id = get_id(pkt[self.i])
                     self.length = pkt[self.i + 1]
                     self.info = pkt[self.i + 2:self.i + 2 + self.length]
@@ -264,9 +260,14 @@ class Dot11:
                             self.ssid = self.info.decode()
                         except:
                             pass
+
                     elif self.id == "Rates" or self.id == "ERates":
                         if self.max_rate < int(self.info[self.length - 1] / 2):
                             self.max_rate = int(self.info[self.length - 1] / 2)
+
+                    elif self.id == "DSSet":
+                        self.channel = get_channel(pkt, self.i, self.length)
+
                     elif self.id == "RSN":
                         self.enc = "WPA2"
                         self.cipher_count = int.from_bytes(pkt[self.i + 8:self.i + 10], byteorder='little')
@@ -274,6 +275,7 @@ class Dot11:
                         if self.oui == b'\x00\x0f\xac':
                             self.cipher = get_cipher(pkt[self.i + 13])
                             self.auth = get_auth(pkt[self.i + 15 + (4 * self.cipher_count)])
+
                     elif self.id == "Vender" and pkt[self.i + 2:self.i + 8] == b'\x00\x50\xf2\x01\x01\x00':
                         self.enc = "WPA"
                         self.cipher_count = int.from_bytes(pkt[self.i + 12:self.i + 14], byteorder='little')
@@ -282,13 +284,17 @@ class Dot11:
                             self.cipher = get_cipher(pkt[self.i + 17])
                             self.auth = get_auth(pkt[self.i + 19 + (4 * self.cipher_count)])
 
+                    elif self.id == "Vender" and pkt[self.i + 2:self.i + 8] == b'\x00\x50\xf2\x02\x01\x01':
+                        self.qos = "e"
+
                     self.i += 2 + self.length
 
-                self.mb = str(self.max_rate) + self.preamble
+                self.mb = str(self.max_rate) + self.qos + self.preamble
 
                 if self.enc == "":
                     if ((pkt[self.fixed_mac_body + 11] & 0b00010000) >> 4) == 1:
                         self.enc = "WEP"
+                        self.cipher = "WEP"
                     elif ((pkt[self.fixed_mac_body + 11] & 0b00010000) >> 4) == 0:
                         self.enc = "OPEN"
 
@@ -297,7 +303,6 @@ class Dot11:
 
         elif self.type == "Data":
             self.sub_type = get_data_sub_type(pkt, self.radiotap_length)
-            self.is_qos_data = get_is_qos_data(pkt, self.radiotap_length)
             self.to_ds = get_to_ds(pkt, self.radiotap_length)
             self.from_ds = get_from_ds(pkt, self.radiotap_length)
             if self.to_ds is False or self.from_ds is False:
--- pyrodump.py
+++ pyrodump.py
@@ -27,78 +27,21 @@ def hopping_channel():
         elif num == 13:
             num = 0
         lock.release()
-        time.sleep(0.3)
+        time.sleep(0.2)
 
 
-def get_ap_list(pkt):
-    ap = []
+def print_result():
+    global ap_list
 
-    dot11 = analysis.Dot11(pkt)
-
-    if dot11.type == "Management":
-        if dot11.sub_type == "Beacon" or dot11.sub_type == "ProbeResp":
-            ap.append(dot11.addr3)  # BSSID
-            ap.append(dot11.pwr)  # PWR
-            ap.append(1)  # Beacons
-            ap.append(0)  # #Data
-            ap.append(0)  # #/s, Number of data packets per second measure over the last 10 seconds.
-            ap.append(dot11.channel)  # CH
-            ap.append(dot11.mb)  # MB
-            ap.append(dot11.enc)  # ENC
-            ap.append(dot11.cipher)  # CIPHER
-            ap.append(dot11.auth)  # AUTH
-            ap.append(dot11.ssid)  # ESSID
-
-            if len(ap_list) != 0:
-                for i in range(len(ap_list)):
-                    if ap_list[i][0] == ap[0]:
-                        if dot11.sub_type == "Beacon":
-                            ap_list[i][2] = ap_list[i][2] + 1
-                            break
-                        else:
-                            break
-
-                    elif i == len(ap_list) - 1:
-                        ap_list.append(ap)
-            else:
-                ap_list.append(ap)
-
-            ap_list.sort(key=lambda x: x[1], reverse=True)
-
-        elif dot11.sub_type == "ProbeReq":
-            pass
-    elif dot11.type == "Data":
-        if len(ap_list):
-            if dot11.from_ds is False or dot11.from_ds is False:
-                for x in range(len(ap_list)):
-                    if ap_list[x][0] == dot11.addr1 or ap_list[x][0] == dot11.addr2 or ap_list[x][0] == dot11.addr3:
-                        ap_list[x][3] += 1
-                        break
-            else:
-                for x in range(len(ap_list)):
-                    if ap_list[x][0] == dot11.addr1 or ap_list[x][0] == dot11.addr2 or ap_list[x][0] == dot11.addr3 or ap_list[x][0] == dot11.addr4:
-                        ap_list[x][3] += 1
-                        break
-
-
-def main(interface):
     start_time = datetime.now()
-
-    t = Thread(target=hopping_channel)
-    t.start()
-
+    lock = Lock()
     scr = curses.initscr()
 
     while True:
-        scr.resize(200, 120)
-
-        pcap = pcapy.open_live(interface, 1024, 0, 0)  # get packet capture descriptor
-        pkt = pcap.next()[1]
-
-        get_ap_list(pkt)
-
         now_time = datetime.now()
         elapsed_time = int((now_time - start_time).total_seconds())
+
+        scr.resize(200, 120)
 
         if elapsed_time < 60:
             scr.addstr(0, 0, "CH  %s ][ Elapsed: %s s ][ %s-%s-%s %s:%s" % (
@@ -117,6 +60,7 @@ def main(interface):
 
         scr.addstr(2, 0, "BSSID              PWR  Beacons    #Data, #/s  CH  MB   ENC  CIPHER AUTH ESSID")
 
+        lock.acquire()
         if len(ap_list) != 0:
             for i in range(len(ap_list)):
                 scr.addstr(4 + i, 0,
@@ -126,9 +70,68 @@ def main(interface):
                            "%-5s" % ap_list[i][9] + "%s" % ap_list[i][10])  # AUTH, ESSID
 
         scr.addstr(5 + len(ap_list), 0, "BSSID              STATION            PWR   Rate    Lost    Frames  Probe")
+        lock.release()
 
         scr.refresh()
+        time.sleep(0.1)
         scr.erase()
+
+
+def get_ap_list(interface):
+    global ap_list
+
+    while True:
+        pcap = pcapy.open_live(interface, 1024, 1000, 0)  # get packet capture descriptor
+        pkt = pcap.next()[1]
+
+        dot11 = analysis.Dot11(pkt)
+
+        if dot11.type == "Management":
+            if dot11.sub_type == "Beacon" or dot11.sub_type == "ProbeResp":
+                """ BSSID, PWR, Beacons, #Data, #/s, CH, MB, ENC, CIPHER, AUTH, ESSID """
+                ap = [dot11.addr3, dot11.pwr, 1, 0, 0, dot11.channel, dot11.mb, dot11.enc, dot11.cipher, dot11.auth, dot11.ssid]
+
+                if len(ap_list) != 0:
+                    for i in range(len(ap_list)):
+                        if ap_list[i][0] == ap[0]:
+                            if dot11.sub_type == "Beacon":
+                                ap_list[i] = [dot11.addr3, dot11.pwr, ap_list[i][2]+1, ap_list[i][3], ap_list[i][4],
+                                              dot11.channel, dot11.mb, dot11.enc, dot11.cipher, dot11.auth, dot11.ssid]
+                                break
+                            else:
+                                ap_list[i] = [dot11.addr3, dot11.pwr, ap_list[i][2], ap_list[i][3], ap_list[i][4],
+                                              dot11.channel, dot11.mb, dot11.enc, dot11.cipher, dot11.auth, dot11.ssid]
+                                break
+
+                        elif i == len(ap_list) - 1:
+                            ap_list.append(ap)
+                else:
+                    ap_list.append(ap)
+
+                ap_list.sort(key=lambda x: x[1], reverse=True)
+
+            elif dot11.sub_type == "ProbeReq":
+                pass
+
+        elif dot11.type == "Data":
+            if len(ap_list):
+                if dot11.from_ds is False or dot11.from_ds is False:
+                    for x in range(len(ap_list)):
+                        if ap_list[x][0] == dot11.addr1 or ap_list[x][0] == dot11.addr2 or ap_list[x][0] == dot11.addr3:
+                            ap_list[x][3] += 1
+                            break
+                else:
+                    for x in range(len(ap_list)):
+                        if ap_list[x][0] == dot11.addr1 or ap_list[x][0] == dot11.addr2 or ap_list[x][0] == dot11.addr3 or ap_list[x][0] == dot11.addr4:
+                            ap_list[x][3] += 1
+                            break
+
+
+def main(interface):
+    Thread(target=hopping_channel).start()
+    Thread(target=print_result).start()
+
+    get_ap_list(interface)
 
 
 if __name__ == "__main__":
